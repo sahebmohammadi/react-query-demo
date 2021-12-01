e384bffd1a75d6225e08483915ab5a504e8ac209
saheb mohammadi <saheb.put92@gmail.com>
2021-12-02 02:04:58 +0330

    traditional fetch

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Posts = () => {
-  return <h2>Posts page</h2>;
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setData(null);
+    setError(null);
+
+    axios
+      .get("http://localhost:3001/posts")
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading && <p>loadig...</p>}
+      {error && <p>{error}</p>}
+      {data && (
+        <div>
+          <h2>Post data</h2>
+          {data.map((post) => (
+            <li key={post.id}>
+              {post.title} - {post.author}
+            </li>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Posts;
